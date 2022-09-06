17732eff5d235ccb1606a192c633c33933f21ebd
Tom Dugan <tom.dugan@affirm.com>
2022-09-06 12:17:49 -0400

    .

--- scripts/auto_tag/auto_tag.py
+++ scripts/auto_tag/auto_tag.py
@@ -1,1 +1,11 @@
-print("hello world")
+from git import Repo
+import os
+from pathlib import Path
+
+path = Path(os.path.dirname(os.path.abspath(__file__)))
+repo = Repo(path, search_parent_directories=True)
+current_commit = repo.head.commit
+main_commit = repo.commit("origin/HEAD")
+staged_tf = [ a.a_path for a in repo.index.diff("HEAD") if 'tfmodule.yaml' in a.a_path ]
+    
+print(staged_tf)
