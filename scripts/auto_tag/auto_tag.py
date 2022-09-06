be0041e3d033e6673d7ba374cc7e80d3236affab
Tom Dugan <tom.dugan@affirm.com>
2022-09-06 12:19:58 -0400

    .

--- scripts/auto_tag/auto_tag.py
+++ scripts/auto_tag/auto_tag.py
@@ -5,7 +5,7 @@ from pathlib import Path
 path = Path(os.path.dirname(os.path.abspath(__file__)))
 repo = Repo(path, search_parent_directories=True)
 current_commit = repo.head.commit
-main_commit = repo.commit("origin/HEAD")
-staged_tf = [ a.a_path for a in repo.index.diff("HEAD") if 'tfmodule.yaml' in a.a_path ]
+main_commit = repo.commit("HEAD")
+staged_tf = [ a.a_path for a in repo.index.diff("HEAD~1") if 'tfmodule.yaml' in a.a_path ]
     
 print(staged_tf)
